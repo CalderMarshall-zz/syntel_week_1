70a7ba807065c235953987a20d185c3ca4fcb8e4
syntel <syntel@PHX440G3-281610.syntelorg.com>
2018-02-26 10:15:14 -0500

    first commit

---
+++ hello_world/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        int thisTotal = add(3);
+        System.out.println("I'm pickle rick!" + thisTotal);
+
+    }
+    public static int add(int num){
+        int total = num + 2;
+        return total;
+    }
+}
